b0796838322f7e73457a4a9e177f60a7974b444f
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2023-11-03 16:23:47 +0100

    dev: added zooming camera area with interpolation

---
+++ Scripts/Camera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+const DEFAULT_ZOOM = Vector2(2,2)
+
+var target_zoom = DEFAULT_ZOOM
+
+func _process(delta):
+	if(self.zoom != target_zoom):
+		self.zoom = lerp(self.zoom, target_zoom, 0.05)
+
+func _on_zoomed_camera_area_body_entered(body):
+	target_zoom = Vector2(4, 4)
+
+
+func _on_zoomed_camera_area_body_exited(body):
+	target_zoom =  DEFAULT_ZOOM
